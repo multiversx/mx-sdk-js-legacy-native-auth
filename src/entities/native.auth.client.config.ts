424683e1db002a66cda396242d898cea542cbfbb
Traian Anghel <tanghel@live.com>
2022-05-23 13:09:50 +0300

    window hostname

--- src/entities/native.auth.client.config.ts
+++ src/entities/native.auth.client.config.ts
@@ -1,5 +1,5 @@
 export class NativeAuthClientConfig {
-  host: string = typeof window !== "undefined" ? new URL(window.location.host).hostname : '';
+  host: string = typeof window !== "undefined" ? window.location.hostname : '';
   apiUrl: string = 'https://api.elrond.com';
   expirySeconds: number = 60 * 60 * 24;
 }
