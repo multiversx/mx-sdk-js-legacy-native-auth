ad7919bbc6dd45eb42f45137e54330e8c564e0d6
Traian Anghel <tanghel@live.com>
2022-05-23 11:15:59 +0300

    proper default for max epiry seconds

--- src/entities/native.auth.server.config.ts
+++ src/entities/native.auth.server.config.ts
@@ -3,6 +3,6 @@ import { NativeAuthCacheInterface } from "../native.auth.cache.interface";
 export class NativeAuthServerConfig {
   apiUrl: string = 'https://api.elrond.com';
   acceptedHosts: string[] = [];
-  maxExpirySeconds: number = 0;
+  maxExpirySeconds: number = 86400;
   cache?: NativeAuthCacheInterface;
 }
